d67f1014968bd56594b4c80539571a7c99bf184d
Matthias Ulrich <post@ulrich.digital>
2022-09-07 16:24:46 +0200

    Update ulrich_digital.js

--- wp-content/themes/ulrichdigital_block_theme/assets/js/ulrich_digital.js
+++ wp-content/themes/ulrichdigital_block_theme/assets/js/ulrich_digital.js
@@ -1,37 +1,3 @@
 jQuery(document).ready(function ($) {
 
-/* =============================================================== *\ 
- 	 Main-Menu 
-\* =============================================================== */ 
-
-$(window).on("load resize",function(e){
-    var $body_width = $("body").width();
-    if($body_width <= 840){
-        $("#main_menu").addClass("animate__animated");    
-        $("#main_menu").addClass("mobile");    
-    }else{
-        $("#main_menu").removeClass("animate__animated");    
-        $("#main_menu").removeClass("mobile");    
-    }
-});
-
-$(".hamburger").on("click", function(){
-    $(this).toggleClass("is-active");
-    $("#main_menu").toggleClass("animate__slideOutRight").toggleClass("animate__slideInRight");
-});
-
-
-
-/* =============================================================== *\ 
- 	 GSAP Config 
-\* =============================================================== */ 
-/*
-gsap.config({
-    nullTargetWarn: false,
-});
-*/
-
-    
-
-
 }); //jQuery(document).ready
