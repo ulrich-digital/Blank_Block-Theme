983490699c728e175c3935dfea0c64cc5d5341be
Matthias Ulrich <matthias@ulrich.digital>
2023-10-11 10:33:33 +0200

    Update ulrich_digital.js

--- wp-content/themes/ulrichdigital_block_theme/assets/js/ulrich_digital.js
+++ wp-content/themes/ulrichdigital_block_theme/assets/js/ulrich_digital.js
@@ -1,3 +1,42 @@
 jQuery(document).ready(function ($) {
-console.log("hello");
+
+/* =============================================================== *\ 
+   Fehlendes title-Attribute hinzufügen
+   - überprüft zuerst den Text-Content
+   - danach das alt-Attribute
+   - danach das Linkziel
+\* =============================================================== */
+
+function fehlendes_title_attribute_hinzufuegen() {
+    $('a').each(function() {
+        var $link = $(this);
+        var title = $link.attr('title');
+        
+        if (!title) {
+            var linkText = $link.text().trim();
+            var imgAlt = $link.find('img').attr('alt');
+
+            if (linkText) {        
+                title = linkText.replace(/\n/g, '').trim().substring(0, 20); // Maximal 20 Zeichen
+            } else if (imgAlt) { 
+                title = imgAlt.replace(/\n/g, '').trim().substring(0, 20); // Maximal 20 Zeichen
+            } else { 
+                var linkHref = $link.attr('href');
+                if (linkHref) {
+                    var linkTitle = linkHref.replace(/^https?:\/\/[^/]+/, '').replace(/\/$/, '').replace(/^\//, '');
+                    title = linkTitle.replace(/\n/g, '').trim().substring(0, 20); // Maximal 20 Zeichen
+                }
+            }
+            
+            if (title) {
+                $link.attr('title', title);
+            }
+        }
+    });
+}
+
+// Aufruf der Funktion
+//fehlendes_title_attribute_hinzufuegen();
+
+
 }); //jQuery(document).ready
