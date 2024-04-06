1a317e512166a08c1fee0a0b4cf3919d2ed486fd
Matthias Ulrich <matthias@ulrich.digital>
2024-04-06 08:16:13 +0200

    Update ulrich_digital.js

--- wp-content/themes/ulrichdigital_block_theme/assets/js/ulrich_digital.js
+++ wp-content/themes/ulrichdigital_block_theme/assets/js/ulrich_digital.js
@@ -1,43 +1,44 @@
 jQuery(document).ready(function ($) {
 
-/* =============================================================== *\ 
-   Fehlendes title-Attribute hinzufügen
-   - überprüft zuerst den Text-Content
-   - danach das alt-Attribute
-   - danach das Linkziel
-\* =============================================================== */
+    /* =============================================================== *\ 
+       Fehlendes title-Attribute hinzufügen
+       - überprüft zuerst den Text-Content
+       - danach das alt-Attribute
+       - danach das Linkziel
+    \* =============================================================== */
 
-function fehlendes_title_attribute_hinzufuegen() {
-    $('a').each(function() {
-        var $link = $(this);
-        var title = $link.attr('title');
+    function fehlendes_title_attribute_hinzufuegen() {
+        $('a').each(function() {
+            var $link = $(this);
+            var title = $link.attr('title');
         
-        if (!title) {
-            var linkText = $link.text().trim();
-            var imgAlt = $link.find('img').attr('alt');
+            if (!title) {
+                var linkText = $link.text().trim();
+                var imgAlt = $link.find('img').attr('alt');
 
-            if (linkText) {        
-                title = linkText.replace(/\n/g, '').trim().substring(0, 20); // Maximal 20 Zeichen
-            } else if (imgAlt) { 
-                title = imgAlt.replace(/\n/g, '').trim().substring(0, 20); // Maximal 20 Zeichen
-            } else { 
-                var linkHref = $link.attr('href');
-                if (linkHref) {
-                    var linkTitle = linkHref.replace(/^https?:\/\/[^/]+/, '').replace(/\/$/, '').replace(/^\//, '');
-                    title = linkTitle.replace(/\n/g, '').trim().substring(0, 20); // Maximal 20 Zeichen
+                if (linkText) {        
+                    title = linkText.replace(/\n/g, '').trim().substring(0, 20); // Maximal 20 Zeichen
+                } else if (imgAlt) { 
+                    title = imgAlt.replace(/\n/g, '').trim().substring(0, 20); // Maximal 20 Zeichen
+                } else { 
+                    var linkHref = $link.attr('href');
+                    if (linkHref) {
+                        var linkTitle = linkHref.replace(/^https?:\/\/[^/]+/, '').replace(/\/$/, '').replace(/^\//, '');
+                        title = linkTitle.replace(/\n/g, '').trim().substring(0, 20); // Maximal 20 Zeichen
+                    }
+                }
+            
+                if (title) {
+                    $link.attr('title', title);
                 }
             }
-            
-            if (title) {
-                $link.attr('title', title);
-            }
-        }
-    });
-}
+        });
+    }
 
-// Aufruf der Funktion
-//fehlendes_title_attribute_hinzufuegen();
+    // Aufruf der Funktion
+    //fehlendes_title_attribute_hinzufuegen();
 
+   
     /* =============================================================== *\ 
        DEVELOPMENT: 
        - Mobile Menü immer geöffnet
