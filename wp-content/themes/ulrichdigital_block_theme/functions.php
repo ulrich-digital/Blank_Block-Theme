75e08a191d50074c79ee83bcc74057392f7046a1
Matthias Ulrich <matthias@ulrich.digital>
2024-09-05 08:04:07 +0200

    Update functions.php
    Add style-reset.css

--- wp-content/themes/ulrichdigital_block_theme/functions.php
+++ wp-content/themes/ulrichdigital_block_theme/functions.php
@@ -645,9 +645,10 @@ function ud_enqueue_frontend_scripts(){
     // wp_dequeue_style('wp-block-column');
 
 	//wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Rubik:ital,wght@0,400;0,500;0,700;1,400;1,500;1,600&display=swap', false );
+	wp_enqueue_style( 'ud-style-reset', get_stylesheet_directory_uri() . "/style-reset.css", [], filemtime( get_stylesheet_directory() . "/style-reset.css" ) );
+	wp_enqueue_style( 'ud-style-main', get_stylesheet_directory_uri() . "/style.css", [], filemtime( get_stylesheet_directory() . "/style.css" ) );
 	wp_enqueue_style( 'ud-style-frontend', get_template_directory_uri() . '/style-frontend.css', array(), wp_get_theme()->get( 'Version' ) );
 	//wp_enqueue_style( 'fontawesome', get_template_directory_uri() . '/assets/fonts/fontawesome/css/all.css', array(), wp_get_theme()->get( 'Version' ) );
-	//wp_enqueue_style( 'main', get_stylesheet_directory_uri() . "/style.css", [], filemtime( get_stylesheet_directory() . "/style.css" ) );
 	//wp_enqueue_style('slick_slider', get_stylesheet_directory_uri() . '/assets/slick/slick.min.css', [], filemtime( get_stylesheet_directory() . "/assets/slick/slick.min.css" ) );
 	//wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/assets/bootstrap-3.4.1/css/bootstrap.min.css', [], filemtime( get_stylesheet_directory() . "/assets/bootstrap-3.4.1/css/bootstrap.min.css" ) );
 	//wp_enqueue_style('bootstrap-slider', get_stylesheet_directory_uri() . '/css/bootstrap-slider.min.css', [], filemtime( get_stylesheet_directory() . "/css/bootstrap-slider.min.css" ) );
